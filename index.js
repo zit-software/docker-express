adb8f2b9d0154a6eac959400805fb4507bbdeac4
Kim Minh Thang <thangved02@gmail.com>
2024-01-06 15:01:04 +0700

    feat: add mongodb & dotenv

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const port = 3000;
+require('dotenv').config();
 
-const app = express();
+/**
+ * @type {number}
+ */
+const port = process.env.PORT || 3000; // Port to run server on
+/**
+ * @type {string}
+ */
+const mongodbUri = process.env.MONGODB_URI; // MongoDB URI
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Server is running',
-        href: 'https://fb.com/zit-software'
-    })
-})
+/**
+ * @returns {Promise<void>}
+ */
+const start = async () => {
+    try {
+        // Try to connect to mongodb
+        await mongoose.connect(mongodbUri);
+        console.log('Connected to mongodb');
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+        // Create express app
+        const app = express();
+
+        // Create routes
+        app.get('/', (req, res) => {
+            res.json({
+                message: 'Server is running',
+                href: 'https://fb.com/zit-software'
+            })
+        })
+
+        // Start server
+        app.listen(port, () => {
+            console.log(`Server started on port ${port}`);
+        });
+    } catch (error) {
+        // If error, log it
+        console.error('Server: ', error);
+
+        // Exit process
+        process.exit(1);
+    }
+};
+
+start(); // Start server
